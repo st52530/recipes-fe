59cde6bc6fd1fb319638697e10e69ec2bd21c0e7
Tomas Kricensky <st52530@student.upce.cz>
2020-04-25 19:06:26 +0200

    Setup .env file

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
     return (
         <div className="App">
             Recepty
+            <p>
+                Api URL: {process.env.REACT_APP_API_URL}
+            </p>
         </div>
     );
 }
