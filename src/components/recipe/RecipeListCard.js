30e9ea0b3f207c85eaaeaa4ba1c54d176af14170
Tomas Kricensky <st52530@student.upce.cz>
2020-05-04 20:32:41 +0200

    Show placeholder image

--- src/components/recipe/RecipeListCard.js
+++ src/components/recipe/RecipeListCard.js
@@ -4,8 +4,8 @@ import {makeStyles} from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
+import placeholder from '../../images/placeholder.svg';
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -60,11 +60,7 @@ const RecipeListCard = ({recipe}) => {
     return (
         <Card className={classes.root}>
             <CardActionArea className={classes.actionArea}>
-                <CardMedia
-                    className={classes.media}
-                    style={{backgroundColor:'red'}}
-                    image="/static/images/cards/contemplative-reptile.jpg"
-                    title={recipe.name}/>
+                <img src={placeholder} className={classes.media} alt={recipe.name}/>
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="h2" className={classes.headline}>
                         {recipe.name}
