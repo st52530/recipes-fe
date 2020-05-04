9acc50e4b81653951c1a1cdab498872f18d2fca5
Tomas Kricensky <st52530@student.upce.cz>
2020-05-04 15:33:48 +0200

    Add logout interceptor

--- src/networking/axiosConfig.js
+++ src/networking/axiosConfig.js
@@ -1,9 +1,20 @@
 import axios from 'axios';
-import {getBearerToken} from "../services/AuthenticationService";
+import {getBearerToken, logout} from "../services/AuthenticationService";
 
 // Create custom instance.
 const instance = axios.create({
     baseURL: process.env.REACT_APP_API_URL
+});
+
+// Add a 401 response interceptor.
+instance.interceptors.response.use(function (response) {
+    return response;
+}, function (error) {
+    if (401 === error.response.status) {
+        // When BE rejects the token logout the user locally.
+        logout()
+    }
+    return Promise.reject(error);
 });
 
 // Setup authentication headers.
