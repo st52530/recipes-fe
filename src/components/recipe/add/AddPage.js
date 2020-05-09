383ae7639e3e75c7049b4485323f5e02784728e1
Tomas Kricensky <st52530@student.upce.cz>
2020-05-09 16:54:43 +0200

    Actually save recipe to server

--- src/components/recipe/add/AddPage.js
+++ src/components/recipe/add/AddPage.js
@@ -1,10 +1,13 @@
 import React, {useState, useEffect} from "react";
+import {withRouter} from "react-router-dom";
 import Typography from '@material-ui/core/Typography';
 import EditRecipeFragment from "../edit/EditRecipeFragment";
 import {makeStyles} from '@material-ui/core/styles';
 import {getCurrentUser} from "../../../services/AuthenticationService";
 import ContentLoadingError from "../../util/ContentLoadingErrror";
 import {getCategories} from "../../../services/CategoryService";
+import {getSlug} from "../../../util/TextUtil";
+import {addRecipe} from "../../../services/RecipeService";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -13,7 +16,7 @@ const useStyles = makeStyles((theme) => ({
     }
 }))
 
-const AddPage = () => {
+const AddPage = ({history}) => {
     const classes = useStyles()
     const currentUser = getCurrentUser()
     const [recipe, setRecipe] = useState({
@@ -34,14 +37,26 @@ const AddPage = () => {
         getAllCategories(setAllCategories, setError)
     }, [])
 
+    const onSuccess = (recipe) => {
+        history.push(`/recipe/${recipe.id}/${getSlug(recipe.name)}`)
+    }
+
+    const tryAgain = () => {
+        if (allCategories == null) {
+            getAllCategories(setAllCategories, setError)
+        } else {
+            addNewRecipe(recipe, image, setError, onSuccess)
+        }
+    }
+
     return (
         <>
             <Typography component="h1" variant="h3" className={classes.headline}>Přidat nový recept</Typography>
-            <ContentLoadingError isLoading={isLoading} tryAgain={() => getAllCategories(setAllCategories, setError)} error={error}>
+            <ContentLoadingError isLoading={isLoading} tryAgain={tryAgain} error={error}>
                 <EditRecipeFragment
                     recipe={recipe}
                     setRecipe={setRecipe}
-                    submitRecipe={addNewRecipe}
+                    submitRecipe={() => addNewRecipe(recipe, image, setError, onSuccess)}
                     image={image}
                     allCategories={allCategories}
                     setImage={setImage}/>
@@ -50,11 +65,15 @@ const AddPage = () => {
     )
 }
 
-export default AddPage
+export default withRouter(AddPage)
 
-async function addNewRecipe() {
-    // TODO!
-    console.log("SAVE!")
+async function addNewRecipe(recipe, image, setError, onSuccess) {
+    try {
+        const data = await addRecipe(recipe, image)
+        onSuccess(data)
+    } catch (e) {
+        setError("Nepodařilo se nahrát nový recept.\nZkuste to prosím znovu.")
+    }
 }
 
 async function getAllCategories(setAllCategories, setError) {
--- src/services/RecipeService.js
+++ src/services/RecipeService.js
@@ -20,3 +20,28 @@ export async function getRecipe(id) {
 export function getRecipeImageUrl(id) {
     return `${process.env.REACT_APP_API_URL}recipes/${id}/image`
 }
+
+export async function addRecipe(recipe, image) {
+    const categories = recipe.categories.map((category) => category.id)
+    const instructions = recipe.instructions.filter((instruction) => instruction.text || instruction.text !== "")
+    const ingredients = recipe.ingredients.filter((ingredient) => ingredient.amount || ingredient.amount !== "")
+        .map((ingredient) => {
+            return {
+                ingredientId: ingredient.ingredient.id,
+                amount: ingredient.amount
+            }
+        })
+    const dto = {
+        name: recipe.name,
+        description: recipe.description,
+        preparationTime: recipe.preparationTime,
+        categories: categories,
+        instructions,
+        ingredients
+    }
+    console.log(dto)
+    setupAuthentication()
+    const response = await axios.post(`recipes`, dto)
+    sessionStorage.setItem(`recipes/${response.data.id}`, JSON.stringify(response.data))
+    return response.data
+}
